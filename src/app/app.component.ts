674020e68ebf9ecb9486896fd9e74dc900f926fc
guynahon <guy.nahon@gmail.com>
2024-05-01 19:21:29 +0300

    getting to learn the synthax better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, ProductsComponent],
   template: `
     <app-home></app-home>
+    <app-products></app-products>
     <router-outlet />
     `,
   styleUrl: './app.component.css'
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,16 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   template: `
-    <ul ng-repeat="p in products">
-      <li>{{p}}</li>
+    <ul *ngFor="let p of products; index as i;">
+      <li class="product">
+        {{p.name}}
+        <button class="plus" (click)="addOne(i)">+</button>
+        <button class="minus" (click)="removeOne(i)">-</button>
+        <div>Total: {{p.total}}</div>
+        <div>Total Price: {{p.productTotalPrice}}$</div>
+      </li>
     </ul>
+    <h1>Total Price: {{totalPrice}}$</h1>
   `,
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-  products = ['tomato', 'cucumber', 'onion', 'apple', 'banana'];
+  products = [
+    {name: 'tomato', total: 0, price: 2, productTotalPrice: 0},
+    {name: 'cucumber', total: 0, price: 1, productTotalPrice: 0},
+    {name: 'onion', total: 0, price: 1, productTotalPrice: 0},
+    {name: 'apple', total: 0, price: 3, productTotalPrice: 0},
+    {name: 'banana', total: 0, price: 4, productTotalPrice: 0}
+  ];
+
+  totalPrice = 0;
+
+
+  addOne(index: number) {
+    this.products[index].total++;
+    this.products[index].productTotalPrice += this.products[index].price;
+    this.totalPrice += this.products[index].price;
+  }
+
+  removeOne(index: number) {
+    if (this.products[index].total > 0) {
+      this.products[index].total--;
+      this.products[index].productTotalPrice -= this.products[index].price;
+      this.totalPrice -= this.products[index].price;
+    }
+  }
 }
